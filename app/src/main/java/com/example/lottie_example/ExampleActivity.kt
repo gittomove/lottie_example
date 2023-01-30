8515a983d5010ea0b38112895dd0668d7db118b4
Maratov Maksat <82010556+gittomove@users.noreply.github.com>
2023-01-30 23:27:40 +0600

    done

---
+++ app/src/main/java/com/example/lottie_example/ExampleActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lottie_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+
+
+
+
+    }
+}
